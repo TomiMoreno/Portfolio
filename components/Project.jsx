bd03d6adc18484d91e54c1e7386a9b4ffb4a7b57
syrfox <tomimoreno03@gmail.com>
2021-05-11 23:24:53 -0300

    Added slideAnimation to projects on scroll

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,63 +1,20 @@
+import ScrollAnimation from "react-animate-on-scroll"
 import ButtonIcon from "./ButtonIcon"
+import styles from '../styles/components/Project.module.css'
 
-export default function Project({name, description, codeURL, pageURL, imageURL }){
-  
+export default function Project({ name, description, codeURL, pageURL, imageURL, isOdd }) {
+
   return <>
-    <div className="split">
-      <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png" alt={name} />
-      <div className="content">
+    <ScrollAnimation className={styles.split} initiallyVisible={false} duration={1} animateOnce={true} animateIn={`slide${isOdd ? 'Right' : 'Left'}`}>
+      <img src={imageURL} alt={name} className={styles.img} />
+      <div className={styles.content}>
         <h2>{name}</h2>
         <p>{description}</p>
-        <div className="btnContainer">
+        <div className={styles.btnContainer} >
           <ButtonIcon href={codeURL} imageURL="/github.svg">Visitar código</ButtonIcon>
           <ButtonIcon href={pageURL} imageURL="/globe.svg">Visitar página</ButtonIcon>
         </div>
       </div>
-    </div>
-    <style jsx>{`
-    .btnContainer{
-      display:flex; 
-      align-items:center;
-      justify-content:center;
-    }
-    h2{
-      color: #289533;
-    }
-    .split{
-      background: #ffffff;
-      border-radius:20px;
-      overflow: hidden;
-      width:60%;
-      display:flex;
-      flex-wrap:wrap;
-      justify-content: center;
-      margin-bottom:2rem;
-      padding: 1rem;
-    }
-    img{
-      object-fit:cover;
-      flex:1;
-      max-width: 20rem;
-      height:auto
-    }
-    .content{
-      flex:1;
-    }
-    .split:nth-child(odd){
-        flex-direction:row-reverse;
-    }
-    @media (min-width:60em){
-      .btnContainer{
-        justify-content: none;
-      }  
-    .split{
-    }
-    
-    
-    
-    
-    }
-      
-    `}</style>
+    </ScrollAnimation>
   </>
 }
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,24 +1,25 @@
 import Project from "./Project"
 
-export default function Projects(){
+export default function Projects() {
   const projectList = [{
-    name:'Btree', 
-    codeURL: 'https://github.com/TomiMoreno/btree', 
-    description: 'Aplicación interactiva de vizualización de árboles binarios.', 
-    pageURL:'btree.vercel.app',
+    name: 'Btree',
+    codeURL: 'https://github.com/TomiMoreno/btree',
+    description: 'Aplicación interactiva de vizualización de árboles binarios.',
+    pageURL: 'btree.vercel.app',
     imageURL: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png',
   },
-  { name:'PreceBOT', 
-    codeURL: '', 
-    description: 'Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion ', 
-    pageURL:'',
+  {
+    name: 'PreceBOT',
+    codeURL: '',
+    description: 'Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion ',
+    pageURL: '',
     imageURL: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png',
   }]
-  return(<>
+  return (<>
     <main>
       <h1>Mis proyectos</h1>
       <div className="container">
-        {projectList.map((project,i)=><Project key={`project-${i}`}{...project} />)}
+        {projectList.map((project, i) => <Project isOdd={i % 2 === 1} key={`project-${i}`} {...project} />)}
       </div>
     </main>
     <style jsx>{`
