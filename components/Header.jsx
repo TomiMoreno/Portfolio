1a409003b3b229d2f3e95ba885746c44d208c2d5
tomiMoreno <tomimoreno03@gmail.com>
2021-05-28 10:16:23 -0300

    Añadiendo call to action

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,22 @@
-import styles from '../styles/components/Header.module.css'
-import Navbar from './Navbar'
+import styles from "../styles/components/Header.module.css";
+import Navbar from "./Navbar";
 export default function Header() {
   return (
     <>
       <header className={styles.header}>
         <div className={styles.container}>
-          <h1 className={styles.title}><span className="accentColor">Tomás</span> Canosa Moreno</h1>
+          <h1 className={styles.title}>
+            <span className="accentColor">Tomás</span> Canosa Moreno
+          </h1>
           <ul className={styles.attributes}>
             <li>💻Web developer</li>
             <li>🌎Bs. As. Argentina</li>
             <li>📅18 años</li>
-            <li>🎩 Mafioso</li>
             <li>📚Estudiante</li>
-            <li>👞Zapatero</li>
           </ul>
+          <a href="#contacto">Contactame!</a>
         </div>
       </header>
     </>
-  )
+  );
 }
