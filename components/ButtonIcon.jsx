b5a1205c555367f04d78acd06148e505e62de311
tomiMoreno <tomimoreno03@gmail.com>
2021-06-21 09:24:22 -0300

    Cambiando paleta de colores

--- components/ButtonIcon.jsx
+++ components/ButtonIcon.jsx
@@ -1,33 +1,11 @@
-export default function ButtonIcon({children, href, imageURL, className}){
-
-  return <>
-    <a href={href} className={className}>{children}<img src={imageURL} /></a>
-    <style jsx>{`
-     
-      a {
-        padding: .5rem .2rem;
-        padding-right: .5rem; 
-        border: solid black 2px;
-        border-radius: 1.5rem;
-        width: 9rem;
-        align-items: center;
-        justify-content: space-between;
-        display: flex; 
-        transition: all .5s;
-        margin-bottom:1em
-      }
-
-      a > img {
-        width: 1.6rem;
-        height: auto;
-        transition: all .5s;
-      }
-      a:hover > img {
-        transform: scale(1.2);
-      }
-      a:hover {
-        border-color: green;
-      }
-    `}</style>
-  </>
+import styles from "../styles/components/ButtonIcon.module.css";
+export default function ButtonIcon({ children, href, imageURL, className }) {
+  return (
+    <>
+      <a href={href} className={`${styles.btn} ${className}`}>
+        {children}
+        <img src={imageURL} />
+      </a>
+    </>
+  );
 }
--- data/Projects.js
+++ data/Projects.js
@@ -1,40 +1,45 @@
-const NEXT_ID = 'nextjs'
-const CSS_ID = 'css'
-const HTML_ID = 'html'
-const JS_ID = 'js'
-const REACT_ID = 'react'
-export const projectList = [{
-  name: 'Btree',
-  codeURL: 'https://github.com/TomiMoreno/btree',
-  description: 'Aplicación interactiva de vizualización de árboles binarios.',
-  pageURL: 'https://btree.vercel.app',
-  imageURL: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png',
-  technologies: [NEXT_ID, CSS_ID, HTML_ID, JS_ID, REACT_ID,]
-},
-{
-  name: 'PreceBOT',
-  codeURL: '',
-  description: 'Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion ',
-  pageURL: '',
-  imageURL: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png',
-  technologies: [NEXT_ID, CSS_ID, HTML_ID, JS_ID, REACT_ID]
-}]
+const NEXT_ID = "nextjs";
+const CSS_ID = "css";
+const HTML_ID = "html";
+const JS_ID = "js";
+const REACT_ID = "react";
+export const projectList = [
+  {
+    name: "Btree",
+    codeURL: "https://github.com/TomiMoreno/btree",
+    description: "Aplicación interactiva de vizualización de árboles binarios.",
+    pageURL: "https://btree.vercel.app",
+    imageURL:
+      "https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png",
+    technologies: [NEXT_ID, CSS_ID, HTML_ID, JS_ID, REACT_ID],
+  },
+  {
+    name: "PreceBOT",
+    codeURL: "",
+    description:
+      "Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion Descripcion descripcion ",
+    pageURL: "",
+    imageURL:
+      "https://media.geeksforgeeks.org/wp-content/cdn-uploads/binary-tree-to-DLL.png",
+    technologies: [NEXT_ID, CSS_ID, HTML_ID, JS_ID, REACT_ID],
+  },
+];
 
 export const technologiesList = {
   [NEXT_ID]: {
-    src: 'technologies/nextjs.svg',
-    name: 'Next.js'
+    src: "technologies/nextjs.svg",
+    name: "Next.js",
   },
   [CSS_ID]: {
-    src: 'technologies/css.svg',
-    name: 'CSS3'
+    src: "technologies/css.svg",
+    name: "CSS3",
   },
   [HTML_ID]: {
-    src: 'technologies/html.svg',
-    name: 'HTML5'
+    src: "technologies/html.svg",
+    name: "HTML5",
   },
   [REACT_ID]: {
-    src: 'technologies/react.png',
-    name: 'React.js'
-  }
-}
+    src: "technologies/react.svg",
+    name: "React.js",
+  },
+};
