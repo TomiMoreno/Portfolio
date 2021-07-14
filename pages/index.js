76593412904e15527d8e84af9619bbe0af07f0fc
tomiMoreno <tomimoreno03@gmail.com>
2021-07-14 14:18:37 -0300

    Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import Head from 'next/head'
-import AboutMe from '../components/AboutMe'
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
-import Projects from '../components/Projects'
+import Head from "next/head";
+import AboutMe from "../components/AboutMe";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Tomás Canosa Moreno</title>
+        <title>Tomás Canosa Moreno | Frontend Developer</title>
+        <meta name="theme-color" content="#222831" />
+        <meta name="description" content="Desarrollador frontend enfofacdo en React y Next" />
+        <link rel="canonical" href="https://tomimoreno.com/" />
+        <meta name="robots" content="index, follow" /> 
+        <meta property="og:title" content="Tomás Canosa Moreno" key="title" />
+        <meta property="og:description" content="Desarrollador frontend enfocado en React y Next" key="description" />
+        <meta property="og:locale" content="es_ES" key="locale" />
+        <meta property="og:type" content="website" key="type" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
@@ -16,5 +24,5 @@ export default function Home() {
       <Projects />
       <AboutMe />
     </>
-  )
+  );
 }
