774eb55563425c8fdcdc1fac412c6c2ed25f1082
Abrocoly <tomimoreno03@gmail.com>
2021-07-16 22:19:47 -0300

    Add photo to header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from "../styles/components/Header.module.css";
-import Navbar from "./Navbar";
 export default function Header() {
   return (
     <>
-      <header className={styles.header}>
+      <header className={`${styles.header} ${styles.split}`}>
+        <div className={styles.circle}></div>
+        <div className={styles.photoContainer} >
+          <img className={styles.photo}src="https://avatars.githubusercontent.com/u/51180401?v=4" alt="Tomás Canosa Moreno" />
+        </div>
         <div className={styles.container}>
           <h1 className={styles.title}>
             <span className="accentColor">Tomás</span> Canosa Moreno
